3a580f0da33802a5bcf079d92df45890d7bfd551
Matteo Maronati <matteo@gentleman-studio.de>
2017-05-05 17:09:23 +0200

    Doc: commenting musicalas class

--- src/js/utils/musicalas.js
+++ src/js/utils/musicalas.js
@@ -8,36 +8,63 @@ import settings from 'settings'
 class Musicalas {
 
   constructor ($el) {
-    // this.paper = paper
+
+    // configuration object for path
     this.pathConfig = {
       width: Math.round(settings.window.x / 4),
       points: 40
     }
+
+    // hook paper to provided el
     paper.setup($el)
-    this.addPath()
+
+    this._addPath()
+
+
     // pass on event handlers
     paper.view.onResize = this.onResize
     paper.view.onMouseMove = this.onMouseMove
   }
 
-  addPath () {
 
-    let points = []
+  // _addPath
+  // ============================================
+  // takes care of drawing a path along a quarter of circle line
+
+  _addPath () {
+
+    // set fir/last point for stencilPath
     let firstPoint = new paper.Segment(paper.view.center.subtract([0, this.pathConfig.width]), null, null)
     let lastPoint = new paper.Segment(paper.view.center.subtract([this.pathConfig.width, 0], null, null))
-    this.line = new paper.Path.Arc({
+
+    // draw an arc (stencilPath)
+    // this is used to get all necessary point along the arc line with getPointAt
+    this.stencilPath = new paper.Path.Arc({
       from: new paper.Point(paper.view.center.subtract([0, this.pathConfig.width])),
       through: new paper.Point(paper.view.center.subtract([this.pathConfig.width, this.pathConfig.width - this.pathConfig.width / 2])),
       to: new paper.Point(paper.view.center.subtract([this.pathConfig.width, 0])),
     })
+
+    // init the poitns toconstruct the real path
+    let points = []
+    // push the firstpoint
     points.push(firstPoint)
+
+    // for items in path config, add points on path at regular distance
+    // distance calculated based on stencilPath lenght and points in pathConfig
     for (var i = 0; i < this.pathConfig.points; i++) {
-      console.log(this.line.getPointAt((this.line.length / this.pathConfig.points) * i));
-      let point = new paper.Segment(new paper.Point(this.line.getPointAt((this.line.length / this.pathConfig.points) * i)), null, null)
+      // calculate
+      let point = new paper.Segment(new paper.Point(this.stencilPath.getPointAt((this.stencilPath.length / this.pathConfig.points) * i)), null, null)
+      // push it
       points.push(point)
     }
+    // push the last point
     points.push(lastPoint)
 
+    // last but not least, add the reall last point in view center
+    points.push(paper.view.center)
+
+    // all ready, add the path
     this.path = new paper.Path({
       segments: points,
       strokeColor: 'black',
@@ -45,12 +72,10 @@ class Musicalas {
       closed: true,
       strokeWidth: 1
     })
-    this.path.add(paper.view.center)
   }
 
   onMouseMove = (e) => {
     _.each(this.path.segments, (segment, index) => {
-      // console.log(index, this.path.segments.length )
       if (index >= 1 && index <= this.path.segments.length - 3) {
         var plusOrMinus = Math.random() < 0.5 ? false : true
         let vector
