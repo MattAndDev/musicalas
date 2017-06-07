8d19bda7eecacda3fd00c447c68d5cdc30a96fd1
Matteo Maronati <me@mattinthe.net>
2017-06-07 14:52:24 +0200

    Rw + doc: reworked audio-parser to be an ee, better method structure, some comments

--- src/js/utils/audio-parser.js
+++ src/js/utils/audio-parser.js
@@ -2,51 +2,113 @@
 // audio-parser.js
 // =======================================================================
 
+// libs
 import _ from 'lodash'
-import settings from 'settings'
+import EventEmitter from 'eventemitter3'
 import Vue from 'Vue'
 import VueResource from 'vue-resource'
+// utils
+import settings from 'settings'
+// env
 import env from 'env'
 
-class AudioParser {
 
-  constructor (streamUrl) {
+
+class AudioParser extends EventEmitter {
+
+  constructor () {
+    // EE are always fancy
+    super()
+    // tell vue to use vue resource
     Vue.use(VueResource)
-    this._testAudioContext((err) => {
-      if (!err) {
-        this._getStream(streamUrl)
-        .then(this._buildSource.bind(this))
-        .then(this._createAalyser.bind(this))
-        .then(() => {})
-      }
-    })
+    // first syncronious check for audiocontext
+    this._testAudioContext()
   }
 
 
-  // _buildSource
+  // setupTrack()
+  // ============================================
+  // takes care of handling requests for new soundcloud stream urls
+  // @params
+  // streamUrl -> string
+  // @emits
+  // ready
+
+  setupTrack (streamUrl) {
+    // get an array bufferFrom provided url
+    this._getArrayBufferFromUrl(streamUrl)
+    // build audioCotnext source from buffer
+    .then(this._buildSourceFromBuffer.bind(this))
+    // reate an analyzer node
+    .then(this._createAalyser.bind(this))
+    // all ready -> ship it
+    .then(() => { this.emit('ready') })
+  }
+
+  // getAverageFrequency()
+  // ============================================
+  // simpla calc of average frequency within range
+  // @params
+  // start -> integer
+  // end -> integer
+  // @returns
+  // total -> integer
+  getAverageFrequency (start, end) {
+    if (isNaN(start) || isNaN(end)) return false
+    let total = 0
+    let length = Math.abs(start, end)
+    for (var i = start; i < end; i++) {
+      total = total + this.context.frequencyData[i]
+    }
+    return parseInt(total / length)
+  }
+
+
+
+  // play()
+  // ============================================
+  // just connecting source to destination
+
+  play () {
+    this.source.connect(this.context.destination)
+  }
+
+  // _buildSourceFromBuffer
   // ============================================
   // builds the source of the adio node
   // @params
   // arrayBuffer -> valid arrayBuffer
 
-  _buildSource () {
+  _buildSourceFromBuffer () {
     return new Promise((resolve, reject) => {
       return this.context.decodeAudioData(this.arrayBuffer, (buffer) => {
         this.source = this.context.createBufferSource()
         this.source.buffer = buffer
         this.source.start(0)
-        console.log('eueue');
         resolve()
       })
     })
   }
 
 
-  // play()
+
+  // _getArrayBufferFromUrl()
+
   // ============================================
-  // just connecting source to destination
-  play () {
-    this.source.connect(this.context.destination)
+  // uses vue-resource to get array buffer from passed url
+  // @params:
+  // streamUrl -> strign
+  // @resolve
+  // response -> response object with arraybuffer
+
+
+  _getArrayBufferFromUrl (streamUrl) {
+    return new Promise((resolve, reject) => {
+      Vue.http.get(streamUrl + `?client_id=${env.scClientId}`, {responseType: 'arraybuffer'}).then(response => {
+        this.arrayBuffer = response.body
+        response.status === 200 ? resolve(response.body /* <- arraybuffer */) : reject(response)
+      })
+    })
   }
 
   // _getStream()
@@ -56,16 +118,6 @@ class AudioParser {
   // streamUrl -> strign
   // @resolve
   // response -> response object with arraybuffer
-
-
-  _getStream (streamUrl) {
-    return new Promise((resolve, reject) => {
-      Vue.http.get(streamUrl + `?client_id=${env.scClientId}`, {responseType: 'arraybuffer'}).then(response => {
-        this.arrayBuffer = response.body
-        response.status === 200 ? resolve(response.body /* <- arraybuffer */) : reject(response)
-      })
-    })
-  }
 
 
   // Set up the Analiser node
@@ -88,17 +140,6 @@ class AudioParser {
   }
 
 
-  getAverageFrequency (start, end) {
-    if (isNaN(start) || isNaN(end)) return false
-    let total = 0
-    let length = Math.abs(start, end)
-    for (var i = start; i < end; i++) {
-      total = total + this.context.frequencyData[i]
-    }
-    return total / length
-  }
-
-
   // _testAudioContext()
   // ============================================
   // check for really old browsrs
@@ -112,11 +153,10 @@ class AudioParser {
       this.context = new AudioContext()
     }
     catch (e) {
-      cb(true)
+      throw e
     }
-    cb(false)
   }
 
 }
 
-export default AudioParser
+export default new AudioParser()
