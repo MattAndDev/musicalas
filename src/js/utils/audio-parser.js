3f5df265f7ef0f48339b8430c31968a2c0102d6f
Matteo Maronati <me@mattinthe.net>
2017-06-07 13:31:49 +0200

    Up: setup of audioparser to accept soundcloud stream_url and return a buffered audio context

--- src/js/utils/audio-parser.js
+++ src/js/utils/audio-parser.js
@@ -4,20 +4,53 @@
 
 import _ from 'lodash'
 import settings from 'settings'
-
+import Vue from 'Vue'
+import VueResource from 'vue-resource'
+import env from 'env'
 
 class AudioParser {
 
-  constructor ($el) {
+  constructor (streamUrl) {
+    Vue.use(VueResource)
+    // Vue.http.options.xhr = {withCredentials: true}
     this._testAudioContext((err) => {
       if (!err) {
-        this._init()
+        this._getStream(streamUrl).then((stream) => {
+          this._buildBuffer(stream)
+        })
       }
     })
   }
 
-  _init () {
-    console.log(this.context);
+  _buildBuffer (stream) {
+    this.context.decodeAudioData(stream.body, (buffer) => {
+      this.source = this.context.createBufferSource()
+      this.source.buffer = buffer
+      this.source.start(0)
+      this.play()
+    })
+  }
+
+
+  play () {
+    this.source.connect(this.context.destination)
+  }
+
+  // _getStream()
+  // ============================================
+  // uses vue-resource to get array buffer from passed url
+  // @params:
+  // streamUrl -> strign
+  // @resolve
+  // response -> response object with arraybuffer
+
+
+  _getStream (streamUrl) {
+    return new Promise((resolve, reject) => {
+      Vue.http.get(streamUrl + `?client_id=${env.scClientId}`, {responseType: 'arraybuffer'}).then(response => {
+        response.status === 200 ? resolve(response) : reject(response)
+      })
+    })
   }
 
   _testAudioContext (cb) {
