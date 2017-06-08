06c7a23893ce50dc99361e302e123e8bfb99f18f
Matteo Maronati <me@mattinthe.net>
2017-06-08 14:21:32 +0200

    Doc + rw: commenting new painter class, better var semantic after rework

--- src/js/utils/painter.js
+++ src/js/utils/painter.js
@@ -12,11 +12,12 @@ import env from 'env'
 
 class Painter {
   constructor ($el) {
-    // configuration object for path
-    this.pathConfig = {
-      width: 5,
+    // configuration object for the circle
+    this.circleConfig = {
+      radius: 5,
       points: 40
     }
+
     // hook paper to provided el
     paper.setup($el)
 
@@ -27,37 +28,42 @@ class Painter {
 
     AudioParser.on('ready', () => {
       this._drawCircle()
-    //   this._addPath()
     })
 
 
   }
 
+  // render
+  // ============================================
+  // pass on for paper render
+
   render () {
     if (this.circle) {
       for (let i = 0; i < this.circle.segments.length; i++) {
         let freq = i <= 20 ? AudioParser.getAverageFrequency(100, 130) : AudioParser.getAverageFrequency(500, 930)
-        let r = this.pathConfig.width + freq
+        let r = this.circleConfig.radius + freq
         let t = 2 * Math.PI * i / this.circle.segments.length
-        let x = Math.round(this.pathConfig.width + r * Math.cos(t))
-        let y = Math.round(this.pathConfig.width + r * Math.sin(t))
+        let x = Math.round(this.circleConfig.radius + r * Math.cos(t))
+        let y = Math.round(this.circleConfig.radius + r * Math.sin(t))
         this.circle.segments[i].point.x = this.circle.rootSegments[i].point.x + x
         this.circle.segments[i].point.y = this.circle.rootSegments[i].point.y + y
       }
     }
   }
-  // _addPath
-  // ============================================
-  // takes care of drawing a path along a quarter of circle line
 
+
+  // _drawCircle
+  // ============================================
+  // creates a ricle with the radius of this.circleConfig.radius
+  // adds n points (this.circleConfig.radius) on the utline of the circle
 
   _drawCircle () {
     let segments = []
-    let r = this.pathConfig.width
-    for (let i = 0; i < this.pathConfig.points; i++) {
-      let t = 2 * Math.PI * i / this.pathConfig.points
-      let x = Math.round(this.pathConfig.width + r * Math.cos(t))
-      let y = Math.round(this.pathConfig.width + r * Math.sin(t))
+    let r = this.circleConfig.radius
+    for (let i = 0; i < this.circleConfig.points; i++) {
+      let t = 2 * Math.PI * i / this.circleConfig.points
+      let x = Math.round(this.circleConfig.radius + r * Math.cos(t))
+      let y = Math.round(this.circleConfig.radius + r * Math.sin(t))
       segments.push(new paper.Segment(new paper.Point(x, y)))
     }
     this.circle = new paper.Path({
@@ -67,12 +73,8 @@ class Painter {
       closed: true,
       strokeWidth: 1
     })
-    this._positionCircle()
+    this.circle.translate(paper.view.center.x - this.circleConfig.radius / 2, paper.view.center.y - this.circleConfig.radius / 2)
     this.circle.rootSegments = _.cloneDeep(this.circle.segments)
-  }
-
-  _positionCircle () {
-    this.circle.translate(paper.view.center.x - this.pathConfig.width / 2, paper.view.center.y - this.pathConfig.width / 2)
   }
 
   onMouseMove = (e) => {
