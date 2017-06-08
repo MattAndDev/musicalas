682a742b05961f572a7c29ecd0adf77a174b9e7e
Matteo Maronati <me@mattinthe.net>
2017-06-08 10:06:15 +0200

    Up: first tests with painter -> segment manipulation

--- src/js/utils/painter.js
+++ src/js/utils/painter.js
@@ -6,6 +6,7 @@ import _ from 'lodash'
 import paper from 'paper'
 // utils
 import settings from 'settings'
+import AudioParser from 'utils/audio-parser'
 // env
 import env from 'env'
 
@@ -19,16 +20,28 @@ class Painter {
     // hook paper to provided el
     paper.setup($el)
 
-    this._addPath()
-
-
     // pass on event handlers
     paper.view.onResize = this.onResize
     paper.view.onMouseMove = this.onMouseMove
+    paper.view.onFrame = this.render.bind(this)
+
+    AudioParser.on('ready', () => {
+      this._addPath()
+    })
+
 
   }
 
-
+  render () {
+    if (this.path) {
+      _.each(this.path.segments, (segment, index) => {
+        if (index === 1) {
+          this.path.segments[index].point.x = this.pathReference[index].point.x - AudioParser.getAverageFrequency(100, 130)
+          this.path.segments[index].point.y = this.pathReference[index].point.y - AudioParser.getAverageFrequency(100, 130)
+        }
+      })
+    }
+  }
   // _addPath
   // ============================================
   // takes care of drawing a path along a quarter of circle line
@@ -74,33 +87,71 @@ class Painter {
       closed: true,
       strokeWidth: 1
     })
+    this.pathReference = _.cloneDeep(this.path.segments)
+    // _.each(this.path.segments, (segment, index) => {
+    //   console.log(segment);
+    // })
+    // _.each(this.path.roots, (segment, index) => {
+    //   console.log(segment);
+    // })
   }
 
+    // _drawPoints (pathCluster) {
+    //   _.each(pathCluster, (path, index) => {
+    //     var angle = index * 2 * Math.PI / path.segments.length - Math.PI / 500;
+    //     // set top left
+    //     let point = new paper.Point(settings.window.x / 2 - AudioParser.getAverageFrequency(100, 200), settings.window.x / 2 - AudioParser.getAverageFrequency(100, 200))
+    //     if (index === 1) {
+    //       path.add(new paper.Point(settings.window.x / 2 - AudioParser.getAverageFrequency(100, 200), settings.window.x / 2 - AudioParser.getAverageFrequency(100, 200)))
+    //     }
+    //     else {
+    //       // path.add(new paper.Point(settings.window.x / 2 + AudioParser.getAverageFrequency(100, 200), settings.window.x / 2 + AudioParser.getAverageFrequency(200, 300)));
+    //     }
+    //   })
+    // }
+    //
+    //
+    //
+    // // _addPath
+    // // ============================================
+    // // takes care of drawing a path along a quarter of circle line
+    //
+    // _addPaths () {
+    //   this.paths = []
+    //   for (var i = 0; i < this.pathConfig.sections; i++) {
+    //     this.paths[i] = []
+    //     for (var y = 0; y < this.pathConfig.sectionRepeaters; y++) {
+    //       this.paths[i][y] = new paper.Path()
+    //       this.paths[i][y].fillColor = new paper.Color(1, 0, 0)
+    //       this.paths[i][y].strokeWidth = 10
+    //     }
+    //   }
+    // }
   onMouseMove = (e) => {
-    _.each(this.path.segments, (segment, index) => {
-      if (index >= 1 && index <= this.path.segments.length - 3) {
-        var plusOrMinus = Math.random() < 0.5 ? false : true
-        let vector
-        let rand1 = Math.floor(Math.random() * 10)
-        let rand2 = Math.floor(Math.random() * 10)
-        if (!plusOrMinus) {
-          vector = segment.point.subtract(new paper.Point(rand1, rand2))
-          vector.length = 2
-          segment.point = segment.point.subtract(vector)
-        }
-        else {
-          vector = segment.point.add(new paper.Point(rand1, rand2))
-          vector.length = 2
-          segment.point = segment.point.add(vector)
-        }
-      }
-    })
-    this.path.segments[0]
-    this.path.smooth({
-      type: 'continuous',
-      from: 1,
-      to: this.path.segments.length - 3
-    })
+    // _.each(this.path.segments, (segment, index) => {
+    //   if (index >= 1 && index <= this.path.segments.length - 3) {
+    //     var plusOrMinus = Math.random() < 0.5 ? false : true
+    //     let vector
+    //     let rand1 = Math.floor(Math.random() * 10)
+    //     let rand2 = Math.floor(Math.random() * 10)
+    //     if (!plusOrMinus) {
+    //       vector = this.roots[index].point.subtract(new paper.Point(rand1, rand2))
+    //       vector.length = 2
+    //       segment.point = segment.point.subtract(vector)
+    //     }
+    //     else {
+    //       vector = segment.point.add(new paper.Point(rand1, rand2))
+    //       vector.length = 2
+    //       segment.point = segment.point.add(vector)
+    //     }
+    //   }
+    // })
+    // this.path.segments[0]
+    // this.path.smooth({
+    //   type: 'continuous',
+    //   from: 1,
+    //   to: this.path.segments.length - 3
+    // })
   }
 
   onResize = (e) => {
