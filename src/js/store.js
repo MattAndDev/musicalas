3e89dcfc56d472125d05c451c89abb40e8c57d61
Matteo Maronati <me@mattinthe.net>
2017-06-09 12:18:54 +0200

    Up: init store -> setter for currentTrack

--- src/js/store.js
+++ src/js/store.js
@@ -3,4 +3,8 @@
 // ============================================
 
 export default {
+  setCurrentTrack (track) {
+    if (this.debug) console.log(`setting current store track with id ${track.id}`)
+    this.currentTrack = track
+  }
 }
