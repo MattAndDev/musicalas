c088bc6c43440eb5cf184a67817a6077b03c07ba
Matteo Maronati <me@mattinthe.net>
2017-06-10 00:45:34 +0200

    Up: testing mirroring

--- src/js/classes/painter.js
+++ src/js/classes/painter.js
@@ -15,8 +15,8 @@ class Painter {
 
     this.pathsConfig = {
       points: 25,
-      sections: 15,
-      children: 14
+      sections: 4,
+      children: 4
     }
 
 
@@ -61,23 +61,40 @@ class Painter {
     _.each(this.paths, (children, mainIndex) => {
       // set reference coordinates
       // each path in group
-      let referenceX = AudioParser.getByteAverageFrequency(this.ranges[mainIndex].x[0], this.ranges[mainIndex].x[1])
-      let referenceY = AudioParser.getByteAverageFrequency(this.ranges[mainIndex].y[0], this.ranges[mainIndex].y[1])
+
       // loop trough children
       _.each(children, (path, index) => {
         // set the first point
-        let referencePoint = new paper.Point(paper.view.center.x - referenceX, paper.view.center.y - referenceY)
-        // calculate degs
-        let deg = index * (360 / this.pathsConfig.children)
-        // rotate the point
-        let point = referencePoint.rotate(deg, paper.view.center)
-        // add it
-        path.add(point)
+        _.each(path, (subPath, subIndex) => {
+          let point = new paper.Point(paper.view.center.x - this.ranges[mainIndex][subIndex].x[subIndex], paper.view.center.y - this.ranges[mainIndex][subIndex].y[subIndex])
+          let deg = index * (360 / this.pathsConfig.children)
+          point = point.rotate(deg, paper.view.center)
+          subPath.add(point)
+          if (subPath.segments.length > this.pathsConfig.points - mainIndex) {
+            subPath.removeSegment(0)
+          }
+        })
+        // let referencepoints = [
+        //
+        // ]
+        // let referencePointA =
+        // let referencePointB = new paper.Point(paper.view.center.x - referenceXB, paper.view.center.y - referenceYB)
+        // // calculate degs
+        // let degA = index * (360 / this.pathsConfig.children)
+        // let degB = index * (360 / this.pathsConfig.children) + (360 / this.pathsConfig.children) / 2
+        // // rotate the point
+        // let pointB = referencePointB.rotate(degB, paper.view.center)
+        // // add it
+        // path[1].add(pointB)
         // smooth it
-        path.smooth()
+        // path[0].smooth({
+        //   type: 'geometric',
+        //   factor: 0.2
+        // })
         // if path has reached it's masximum length, remove last
-        if (path.segments.length > this.pathsConfig.points - mainIndex) {
-          path.removeSegment(0)
+        if (path[0].segments.length > this.pathsConfig.points - mainIndex) {
+          path[0].removeSegment(0)
+          path[1].removeSegment(0)
         }
       })
     })
@@ -98,7 +115,7 @@ class Painter {
     // NOTE: we want to analyze a range between 0 and 10000 Hz.
     // therefore the follwoing: range / each range of array (24) / numebr of sections
     let step = (15000 / AudioParser.arrayBandwidth) / this.pathsConfig.sections  // should be about 100 herz
-    console.log('each path analyzes with increment of' + (step * AudioParser.arrayBandwidth) );
+    console.log('each path analyzes with increment of' + (step * AudioParser.arrayBandwidth) )
     let lastStep = 0
     // iterate trugh paths
     for (var i = 0; i < this.pathsConfig.sections; i++) {
@@ -106,23 +123,22 @@ class Painter {
       this.paths[i] = []
       // assign to every section an x/y analyzer point
       // let randIncrement = Math.floor(Math.random() * 300) + 1
-      this.ranges[i] = {
-        x: [
-          lastStep,
-          lastStep + step / 2
-        ],
-        y: [
-          lastStep + step / 2,
-          lastStep + step
-        ]
-      }
+      this.ranges[i] = [{
+        x: [ lastStep, lastStep + step / 2 ],
+        y: [ lastStep + step / 2, lastStep + step ]
+      },
+      {
+        y: [ lastStep, lastStep + step / 2 ],
+        x: [ lastStep + step / 2, lastStep + step ]
+      }]
       lastStep = lastStep + step
       // loop trough children and just scaffold path
       for (var y = 0; y < this.pathsConfig.children; y++) {
-        this.paths[i][y] = new paper.Path()
-        this.paths[i][y].fillColor = 'black'
-        this.paths[i][y].strokeWidth = 1
-        this.paths[i][y].smooth()
+        let pathConfig = {
+          fillColor: 'black',
+          strokeWidth: 1
+        }
+        this.paths[i][y] = [new paper.Path(pathConfig), new paper.Path(pathConfig)]
       }
     }
   }
