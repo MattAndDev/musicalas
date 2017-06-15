f44a84a64b7c80533073b4bf114377e997eb4c2d
Matteo Maronati <me@mattinthe.net>
2017-06-16 00:15:35 +0200

    Up: iplemented cookie to store all generated ids as well as the current one close #11

--- src/js/classes/mvcke-api.js
+++ src/js/classes/mvcke-api.js
@@ -2,16 +2,13 @@
 // Paper.js
 // =======================================================================
 // libs
-import _ from 'lodash'
-import paper from 'paper'
 import Vue from 'Vue'
 import VueResource from 'vue-resource'
 import hash from 'string-hash'
+import Cookie from 'js-cookie'
 // classes
 import Painter from 'classes/painter'
 import AudioParser from 'classes/audio-parser'
-// utils
-import settings from 'utils/settings'
 // env
 import env from 'env'
 // store
@@ -60,6 +57,7 @@ class MvckeApi {
     Painter.$el.toBlob((blob) => {
       var formdata = new FormData()
       formdata.append('file', blob, timeStamp)
+      // ship it
       Vue.http.post(`${env.apiEndpoint}/track/save/png/${this.id}`, formdata, {
         headers: { 'Content-Type': 'multipart/form-data;' }
       }).then(res => { return true }, err => { throw err })
@@ -102,9 +100,27 @@ class MvckeApi {
     }
     Vue.http.post(`${env.apiEndpoint}/track/register/${this.id}`, data, {
       headers: { 'Content-Type': 'application/json; charset=utf-8' }
-    }).then((res) => {
-      // console.log(res)
-    })
+    }).then(res => {
+      this._setCookieID()
+      return true
+    },
+      err => { throw err })
+  }
+
+  _setCookieID () {
+    if (Cookie.get('track-ids')) {
+      let ids = JSON.parse(Cookie.get('track-ids'))
+      ids.push(this.id)
+      console.log(ids);
+      JSON.stringify(ids)
+      Cookie.set('track-ids', ids)
+      // do something with cookie
+    }
+    else {
+      let ids = JSON.stringify([this.id])
+      Cookie.set('track-ids', ids)
+    }
+    Cookie.set('current-track-id', this.id)
   }
 
 
